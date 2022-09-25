9582f6cb7c347b26644e2e6ae51374f736dca57b
Praveen Saravanan <praveensaravananoff@gmail.com>
2022-09-25 20:36:28 +0530

    ScoreBoard added

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useMemo, useState } from "react";
-import { Colors, initPiecesMap } from "../constant";
+import React, { useEffect, useLayoutEffect, useMemo, useState } from "react";
+import { Colors, initPiecesMap, PieceMap } from "../constant";
 import * as Libs from "../libs";
 import styles from "./Board.module.css";
 import { Square } from "./Square";
@@ -9,6 +9,8 @@ export const Board = () => {
   const [pieces, setPieces] = useState(initPiecesMap);
   const [turn, setTurn] = useState<Colors>(Colors.WHITE);
   const [possibleMoves, setPossibleMoves] = useState<Set<number>>(new Set());
+  const [scale, setScale] = useState("1");
+  const [removedPeices, setRemovedPeices] = useState<PieceMap[number][]>([]);
 
   const updateSelectedSquare = (id: number | null) => {
     if (id === null || selectedSquare === id) setSelectedSquare(null);
@@ -22,6 +24,8 @@ export const Board = () => {
   const movePeice = (from: number, to: number) => {
     if (pieces[from]?.color === pieces[to]?.color) return;
     if (possibleMoves.has(to)) {
+      if (pieces[to]) setRemovedPeices((prev) => [...prev, pieces[to]]);
+
       setPossibleMoves(new Set());
       setPieces((prev) => {
         const newPieces = { ...prev };
@@ -60,11 +64,57 @@ export const Board = () => {
     );
   }, [selectedSquare]);
 
-  const scale = String(Math.min(window.innerWidth, window.innerHeight) / 900);
+  useLayoutEffect(() => {
+    // const scale = String(Math.min(window.innerWidth, window.innerHeight) / 900);
+
+    const updateSize = () =>
+      setScale(String(Math.min(window.innerWidth, window.innerHeight) / 900));
+
+    window.addEventListener("resize", updateSize);
+
+    updateSize();
+
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
+
+  const ChessBoard = () => {
+    return <div className={styles.board}>{squares}</div>;
+  };
 
   return (
-    <div className={styles.board} style={{ scale }}>
-      {squares}
+    <div className={styles.boardWrapper} style={{ scale }}>
+      <ScoreBoard turn={turn} removedPeices={removedPeices} />
+      <ChessBoard />
+    </div>
+  );
+};
+
+interface ScoreBoardProps {
+  turn: Colors;
+  removedPeices: PieceMap[number][];
+}
+
+const ScoreBoard = ({ turn, removedPeices }: ScoreBoardProps) => {
+  const Score = ({ colour }: { colour: Colors }) => {
+    return (
+      <div className={`${turn === colour ? styles.active : ""}`}>
+        <h2>{colour}</h2>
+        <h4>Points</h4>
+        {/* <h3>{Libs.getPoints(pieces, Colors.WHITE)}</h3> */}
+
+        <div className={styles.removedPeicesContainer}>
+          {removedPeices.map((piece) => {
+            if (piece.color !== colour) return <img src={piece.img} alt="" />;
+          })}
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div className={styles.scoreBoard}>
+      <Score colour={Colors.WHITE} />
+      <Score colour={Colors.BLACK} />
     </div>
   );
 };
